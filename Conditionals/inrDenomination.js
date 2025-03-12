a4f5582944876e82587f211f140a5596d68e5a98
ddas4548 <ddas4548@gmail.com>
2025-03-12 13:29:11 +0530

    inr denomination problem is added

---
+++ Conditionals/inrDenomination.js
@@ -0,0 +1,47 @@
+let inr =7931;
+
+if(inr>=500){
+    let r=Math.floor((inr/500))
+    console.log("500 ki note hoga " + r);
+    inr = Math.floor((inr%500))
+}
+if(inr>=200){
+    let r=Math.floor((inr/200))
+    console.log("200 ki note hoga " + r);
+    inr = Math.floor((inr%200))
+}
+if(inr>=100){
+    let r=Math.floor((inr/100))
+    console.log("100 ki note hoga " + r);
+    inr = Math.floor((inr%100))
+}
+if(inr>=50){
+    let r=Math.floor((inr/50))
+    console.log("50 ki note hoga " + r);
+    inr = Math.floor((inr%50))
+}
+if(inr>=20){
+    let r=Math.floor((inr/20))
+    console.log("20 ki note hoga " + r);
+    inr = Math.floor((inr%20))
+}
+if(inr>=10){
+    let r=Math.floor((inr/10))
+    console.log("10 ki note hoga " + r);
+    inr = Math.floor((inr%10))
+}
+if(inr>=5){
+    let r=Math.floor((inr/5))
+    console.log("5 ki note hoga " + r);
+    inr = Math.floor((inr%5))
+}
+if(inr>=2){
+    let r=Math.floor((inr/2))
+    console.log("2 ki note hoga " + r);
+    inr = Math.floor((inr%2))
+}
+if(inr>=1){
+    let r=Math.floor((inr/1))
+    console.log("1 ki note hoga " + r);
+    inr = Math.floor((inr%1))
+}
